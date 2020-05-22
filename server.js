06f32db820594657b4ed32eb818df25bea18087c
Aituar <aituufs@gmail.com>
2020-05-22 16:38:54 +0200

    Creating a simple Tour Model

--- server.js
+++ server.js
@@ -16,7 +16,24 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => console.log('Connected to DB...'));
-  
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour  must have a price'],
+  },
+  rating: {
+    type: Number,
+    default: '4.5',
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const app = require('./app');
 
 const port = process.env.PORT || 5000;
