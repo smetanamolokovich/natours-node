d2d3c1dbba170b29de074b2e7d55f646e7b4b75a
Aituar <aituufs@gmail.com>
2020-05-23 23:02:27 +0200

    Importing Development Data Script

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,50 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const Tour = require('../../model/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DB_HOST.replace(
+  '<PASSWORD>',
+  process.env.DB_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to DB...'));
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully imported');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
