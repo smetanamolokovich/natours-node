b5b67c522a792695784ab34bfdb2d4d594792fb9
Aituar <aituufs@gmail.com>
2020-05-24 03:58:54 +0200

    Making the API better: Limiting Fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -32,6 +32,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // 3) Fields Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     // Execute query
     const tours = await query;
 
