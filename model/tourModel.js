7040d35e67e54fc0f5db677abae99517e82f3db7
Aituar <aituufs@gmail.com>
2020-05-27 13:13:23 +0200

    Data Validation: Built-In Validators

--- model/tourModel.js
+++ model/tourModel.js
@@ -8,6 +8,14 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
       required: [true, 'A tour must have a name'],
+      maxlength: [
+        40,
+        'A tour name must have less than 40 characters',
+      ],
+      minlength: [
+        10,
+        'A tour name must have at least 10 characters',
+      ],
     },
     duration: {
       type: Number,
@@ -20,6 +28,11 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message:
+          'Difficulty is either: easy, medium or difficult',
+      },
     },
     price: {
       type: Number,
@@ -28,6 +41,8 @@ const tourSchema = new mongoose.Schema(
     ratingsAverage: {
       type: Number,
       default: '4.5',
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
