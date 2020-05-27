a2f0f58d4e7e963b7596aa374eea5422b44f1cee
Aituar <aituufs@gmail.com>
2020-05-27 12:56:04 +0200

    Aggregation Middleware

--- model/tourModel.js
+++ model/tourModel.js
@@ -88,6 +88,15 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
+// Aggregation middleware
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({
+    $match: { secretTour: { $ne: true } },
+  });
+
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
