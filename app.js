fb4ec40a756c736bdd2eb3c473ca175d24d01f99
Aituar <aituufs@gmail.com>
2020-05-28 20:24:39 +0200

    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -23,4 +23,14 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// Handling Unhandled Routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Cannot find ${req.originalUrl} on this server!`,
+  });
+
+  next();
+});
+
 module.exports = app;
