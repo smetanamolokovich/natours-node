0a62e9dcedcbc4b95a917888277ed6b19f46283e
Aituar <aituufs@gmail.com>
2020-05-29 17:05:23 +0200

    Handling Invalid Database IDs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/AppError');
+
+const handleCastErrorDB = (error) => {
+  const message = `Invalid ${error.path}: ${error.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -33,6 +40,11 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+
+    if (error.name === 'CastError')
+      error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
