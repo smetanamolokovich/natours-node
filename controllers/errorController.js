df24b76366b750640d35a472910223fd8fd2767e
Aituar <aituufs@gmail.com>
2020-05-29 22:13:10 +0200

    Handling Duplicate Database Fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,14 @@ const handleCastErrorDB = (error) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (error) => {
+  const value = error.errmsg.match(
+    /(["'])(?:(?=(\\?))\2.)*?\1/
+  )[0];
+  const message = `Duplicate field value ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -44,6 +52,8 @@ module.exports = (err, req, res, next) => {
 
     if (error.name === 'CastError')
       error = handleCastErrorDB(error);
+    if (error.code === 11000)
+      error = handleDuplicateFieldsDB(error);
 
     sendErrorProd(error, res);
   }
