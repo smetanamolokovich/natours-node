1f7b8ae9247eb6fcc13c6dbe40f5525036f32f67
Aituar <aituufs@gmail.com>
2020-05-29 22:37:41 +0200

    Errors Outside Express: Unhandled Rejections

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ const app = require('./app');
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Running server on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
