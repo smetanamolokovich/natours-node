4b6a0299b06969bd5dbeb445e8d808876369f0a7
Aituar <aituufs@gmail.com>
2020-05-29 22:47:58 +0200

    Catching Uncaught Exceptions

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', (err) => {
+  console.log(`${err.name}: ${err.message}`);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DB_HOST.replace(
@@ -26,7 +31,7 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
+  console.log(`${err.name}: ${err.message}`);
   server.close(() => {
     process.exit(1);
   });
