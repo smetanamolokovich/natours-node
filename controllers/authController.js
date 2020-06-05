012c34fefa6a335ae39187394bcc15f56eedc108
Aituar <aituufs@gmail.com>
2020-06-05 03:03:25 +0200

    Password Reset Functionality: Reset Token

--- controllers/authController.js
+++ controllers/authController.js
@@ -123,3 +123,22 @@ exports.restrictTo = (...roles) => {
     next();
   };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // Get user based on POSTed email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(
+      new AppError(`There is no user with this email address.`, 404)
+    );
+  }
+
+  // Generate the random reset
+
+  const resetToken = user.createPasswordResetToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  // Send it to user's email
+});
+exports.resetPassword = (req, res, next) => {};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,9 @@ const router = express.Router();
 router.post('/sign-up', authController.signUp);
 router.post('/sign-in', authController.signIn);
 
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
+
 router
   .route('/')
   .get(userController.getAllUsers)
