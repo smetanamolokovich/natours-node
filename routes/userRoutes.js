89c669159244617180f0b4afa670f50c2ad394d9
Aituar <aituufs@gmail.com>
2020-06-05 14:20:19 +0200

    Sending Emails with Nodemailer

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,7 @@ router.post('/sign-up', authController.signUp);
 router.post('/sign-in', authController.signIn);
 
 router.post('/forgot-password', authController.forgotPassword);
-router.post('/reset-password', authController.resetPassword);
+router.patch('/reset-password:/token', authController.resetPassword);
 
 router
   .route('/')
