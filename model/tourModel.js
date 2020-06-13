34335da9b81e520518bee71af9cdc820ebb7285a
Aituar <aituufs@gmail.com>
2020-06-13 21:36:19 +0200

    Revert "Modelling Tour Guides: Child Referencing"
    This reverts commit 411be082ac84f96908841c65201f74b9a2973180.

--- model/tourModel.js
+++ model/tourModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -104,7 +103,6 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
-    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -120,15 +118,6 @@ tourSchema.virtual('durationWeeks').get(function () {
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
 
-  next();
-});
-
-tourSchema.pre('save', async function (next) {
-  const guidesPromises = this.guides.map(
-    async (id) => await User.findById(id)
-  );
-
-  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
