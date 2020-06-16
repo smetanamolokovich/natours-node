9bf39cc5b83744c3c3a256b5899befb79912ba48
Aituar <aituufs@gmail.com>
2020-06-16 12:49:22 +0200

    Virtual Populate: Tours and Reviews

--- model/tourModel.js
+++ model/tourModel.js
@@ -120,6 +120,12 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id',
+});
+
 // Document middleware: "pre" runs before .save() and .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
