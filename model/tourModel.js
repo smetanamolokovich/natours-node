70c75d28cd822f5b62300d05c5e552dfb9583883
Aitu <aituufs@gmail.com>
2020-06-25 18:20:05 +0200

    Improving Read Performance with Indexes

--- model/tourModel.js
+++ model/tourModel.js
@@ -120,6 +120,9 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 tourSchema.virtual('reviews', {
   ref: 'Review',
   foreignField: 'tour',
