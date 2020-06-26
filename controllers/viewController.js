b90c313b322389b2cdba806a52890182ba8158d5
Aitu <aituufs@gmail.com>
2020-06-26 23:37:02 +0200

    Building the Tour Overwiew

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,14 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../model/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+
   res.status(200).render('pages/overview', {
     title: 'All tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('pages/tour', {
