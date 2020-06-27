cdb2d209a5cf84b357707d546c03bbbd9af40a63
Aitu <aituufs@gmail.com>
2020-06-27 05:28:27 +0200

    Building the Tour Page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -10,8 +10,16 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate(
+    {
+      path: 'reviews',
+      fields: 'review, rating, user',
+    }
+  );
+
   res.status(200).render('pages/tour', {
-    title: 'The Forrest Hiker',
+    title: tour.name,
+    tour,
   });
-};
+});
