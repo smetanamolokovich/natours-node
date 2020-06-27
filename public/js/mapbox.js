caaa2049db94d7eb56cc10e3c414ed24cea4ee67
Aitu <aituufs@gmail.com>
2020-06-27 12:47:21 +0200

    Including a Map with Mapbox

---
+++ public/js/mapbox.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+
+const locations = JSON.parse(
+  document.getElementById('map').dataset.locations
+);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibWFzdGVyd29yazMzIiwiYSI6ImNrYnhobzgzcjBxM2wycHRieTR2NjQyOTIifQ.EdXmodnqsqTgen27EaOYrw';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/masterwork33/ckbxhtbr10lna1imnrwyftehb',
+  scrollZoom: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
