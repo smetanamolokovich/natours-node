b3831f1570976266f4fd527e18712b6af56e305d
Aitu <aituufs@gmail.com>
2020-06-29 20:26:30 +0200

    Updating User Password with API

--- public/js/index.js
+++ public/js/index.js
@@ -3,12 +3,15 @@ import '@babel/polyfill';
 import 'regenerator-runtime/runtime';
 
 import { login, logout } from './login';
-import { updateUserData } from './updateUserData';
+import { updateSettings } from './updateSettings';
 import { displayMap } from './mapbox';
 
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const updateUserDataForm = document.querySelector('.form-user-data');
+const updateUserPasswordForm = document.querySelector(
+  '.form-user-settings'
+);
 const logoutBtn = document.querySelector('.nav__el--logout');
 
 if (mapBox) {
@@ -32,11 +35,45 @@ if (logoutBtn) {
 }
 
 if (updateUserDataForm) {
-  updateUserDataForm.addEventListener('submit', (e) => {
+  updateUserDataForm.addEventListener('submit', async (e) => {
     e.preventDefault();
+
+    document.querySelector('.btn--save-data').innerHTML =
+      'Updating...';
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
 
-    updateUserData(name, email);
+    await updateSettings({ name, email }, 'data');
+
+    document.querySelector('.btn--save-data').innerHTML =
+      'Data saved';
+  });
+}
+
+if (updateUserPasswordForm) {
+  updateUserPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').innerHTML =
+      'Updating...';
+
+    const passwordCurrent = document.getElementById(
+      'password-current'
+    ).value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById(
+      'password-confirm'
+    ).value;
+
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    document.querySelector('.btn--save-password').innerHTML =
+      'Password saved';
+
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
 }
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -5,18 +5,23 @@ import 'regenerator-runtime/runtime';
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-export const updateUserData = async (name, email) => {
+export const updateSettings = async (data, type) => {
+  const endpoint = `${
+    type === 'password' ? 'update-my-password' : 'update-me'
+  }`;
   try {
     const res = await axios.patch(
-      `http://localhost:3000/api/v1/users/update-me`,
-      {
-        name,
-        email,
-      }
+      `http://localhost:3000/api/v1/users/${endpoint}`,
+      { ...data }
     );
 
     if (res.data.status === 'success') {
-      showAlert('success', 'Updated successfully!');
+      showAlert(
+        'success',
+        `${
+          type === 'password' ? 'Password' : 'Data'
+        } updated successfully!`
+      );
       // window.setTimeout(() => {
       //   location.assign('/me');
       // }, 500);
