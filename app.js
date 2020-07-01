9442f4f9fc5c2fc5517165e3071a8c413572af95
Aitu <aituufs@gmail.com>
2020-07-01 11:06:48 +0200

    Integrating Stripe into the Back-End

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
+
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -36,8 +38,7 @@ if (process.env.NODE_ENV === 'development') {
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message:
-    'Too many request from this IP, please try again in an hour!',
+  message: 'Too many request from this IP, please try again in an hour!',
 });
 
 app.use('/api', limiter);
@@ -77,18 +78,15 @@ app.use((req, res, next) => {
 
 // ROUTES
 app.use('/', viewRouter);
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/booking', bookingRouter);
 
 // Handling Unhandled Routes
 app.all('*', (req, res, next) => {
-  next(
-    new AppError(
-      `Cannot find ${req.originalUrl} on this server!`,
-      404
-    )
-  );
+  next(new AppError(`Cannot find ${req.originalUrl} on this server!`, 404));
 });
 
 // Global Error Handling Middleware
